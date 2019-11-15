907f4910796403cd92529f00ecfca9ef078f00fe
Bryce Martin <bjmartin@mail.lipscomb.edu>
2019-11-15 13:22:43 -0600

    started implimentation of login components.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+public email: string  = "";
+public password: string = "";
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    //if already logged in
+    if(localStorage.getItem('user')){
+      this.router.navigate(['/products'])
+    }
+  }
+
+  login(){
+    //this should be done in the backend or firebase
+    //and encrypt your password
+    if(this.email == "admin" && this.password == "password"){
+      localStorage.setItem("user", this.email);
+      this.router.navigate(['/admin']);
+    }
+    else
+    {
+      this.router.navigate(['/login']);
+    }
+
   }
 
 }
