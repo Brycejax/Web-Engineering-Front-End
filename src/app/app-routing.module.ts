803b567e72709b2de9e718ef125d984c88888882
Alex Nordhoff <anordhoffkid@gmail.com>
2019-11-15 13:42:32 -0600

    added admin routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import {ProductsComponent} from './products/products.component'
+import {ProductsComponent} from './products/products.component';
+import {AdminComponent} from './admin/admin.component';
 
 
 const routes: Routes = [
@@ -16,7 +17,11 @@ const routes: Routes = [
   {
     path : '',
     component:ProductsComponent
-    }
+  },
+  {
+    path:'admin',
+    component: AdminComponent
+  }
 ];
 
 @NgModule({
