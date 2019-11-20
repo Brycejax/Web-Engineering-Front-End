0464962516e076e2c22da4677280638505a3d8fe
Alex Nordhoff <anordhoffkid@gmail.com>
2019-11-20 13:11:13 -0600

    fixed about services and contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './login/login.component';
 import {ProductsComponent} from './products/products.component';
 import {AdminComponent} from './admin/admin.component';
 import{RegisterComponent} from './register/register.component';
+import{ContactComponent} from './contact/contact.component';
+import{AboutComponent} from './about/about.component';
+import{ServicesComponent} from './services/services.component';
 
 
 const routes: Routes = [
@@ -26,6 +29,18 @@ const routes: Routes = [
   {
     path:'register',
     component: RegisterComponent
+  },
+  {
+    path: 'services',
+  component: ServicesComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
   }
 ];
 
