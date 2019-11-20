418603c3a8439b26a94d67c6bd653b46fc4b5080
Alex Nordhoff <anordhoffkid@gmail.com>
2019-11-20 13:06:42 -0600

    added products services home and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { AdminComponent } from './admin/admin.component';
 import { FormsModule } from '@angular/forms'
 
 import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,10 @@ import { HttpClientModule } from '@angular/common/http';
     RegisterComponent,
     ProductsComponent,
     FooterComponent,
-    AdminComponent
+    AdminComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
